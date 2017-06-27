1bdcad4af1c852ff2994d5e38c045bcbef993b39
dnylng <iamdannyluong@gmail.com>
2017-06-27 15:12:44 -0400

    Initial commit

---
+++ LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeguePractice
+//
+//  Created by Danny Luong on 6/26/17.
+//  Copyright © 2017 dnylng. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
